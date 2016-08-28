25136abd01c4db63f01187cb06be62b2a28d73b7
Silvana Goberdhan-Vigle <silvanagoberdhanvigle@gmail.com>
2016-08-28 10:27:44 -0400

    partially completed

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,8 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-
+    @song.artist = song_params[:artist_name]
+    song_artist_name = @song.artist
     if @song.save
       redirect_to @song
     else
@@ -47,7 +48,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_name, :note_contents)
   end
 end
-
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,28 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    @artist_name
+  end
+
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    @genre_name
+  end
+
+  def note_ids=(ids)
+    ids.each do |id|
+      note = Note.find(id)
+      self.notes << note
+    end
+  end
+end
